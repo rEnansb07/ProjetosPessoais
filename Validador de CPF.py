9e05a1395ee32bd1ccdbda2e88539ac740d7c4ca
Renan da Silva Batista <renans.batista07@gmail.com>
2026-07-20 15:44:41 -0300

    Percebi um erro de lógica quando o CPF possuía números idênticos

--- Validador de CPF.py
+++ Validador de CPF.py
@@ -2,23 +2,22 @@ from ex111.utilidadesCeV import dados
 multiplicadores = 10
 multiplicadores2 = 11
 soma = igualdade = repeticao = 0
-cpf = str(dados.leiaint('Insira seu CPF: ')) #Tratamento de exceções caso o CPF não seja um número
-lista = cpf.split() #Separação do CPF em algarismos
-#print(lista)
+cpf = str(dados.leiaint('Insira seu CPF: ')) # Tratamento de exceções caso o CPF não seja um número
+lista = cpf.split() # Separação do CPF em algarismos
 if len(cpf) == 10: # Tratando o bug quando o CPF começa com zero
     cpf = str('0') + lista[0]
     lista.append(cpf)
     lista.pop(0)
     igualdade = 0
-    print(lista)
-elif len(cpf) != 11 and not len(cpf) == 10:
+elif len(cpf) != 11 and not len(cpf) == 10: # Loop caso o cpf inserido anteriormente não esteja com 11 algarismos ou não tenha 10 algarismos
     while len(cpf) != 11:
         print('Insira corretamente o seu CPF: ')
         cpf = str(dados.leiaint('Insira seu CPF: '))
         lista = cpf.split()
-        if len(cpf) == 10:
-            lista = str('0') + lista[0]
-            print(lista)
+        if len(cpf) == 10: # Tratando o bug do 0 dentro do loop
+            cpf = str('0') + lista[0]
+            lista.append(cpf)
+            lista.pop(0)
             igualdade = 0
             break
 if len(cpf) == 11:
@@ -41,26 +40,28 @@ if repeticao != 11:
     print(lista[0][9])
     print(validacao)
     if int(lista[0][9]) != validacao:
-        print('CPF inválido1')
+        print('CPF inválido!')
+    else:
+        soma = 0 # Redefinição da variável pra segunda validação
+        multiplicadores = 11
+        for item in lista:
+            for numero in item: # Realização da verificação do segundo dígito através da soma
+                if multiplicadores == 1:
+                    break
+                numero = int(numero)
+                soma = soma + (numero * multiplicadores)
+                multiplicadores -= 1
+        validacao2 = ((soma * 10) % 11)
+        if validacao2 == 10:
+            validacao2 = 0
+        #print(lista[0][10])
+        #print(validacao2)
+        if int(lista[0][10]) != validacao2:
+            print('CPF inválido!')
+        else:
+            print('CPF totalmente validado com sucesso!')
 else:
-    print('CPF inválido!2')
-soma = 0 # Redefinição da variável pra segunda validação
-for item in lista:
-    for numero in item:
-        if multiplicadores2 == 1:
-            break
-        numero = int(numero)
-        soma = soma + (numero * multiplicadores2)
-        multiplicadores2 -= 1
-validacao2 = ((soma * 10) % 11)
-if validacao2 == 10:
-    validacao2 = 0
-print(soma)
-#print(lista[0][10])
-#print(validacao2)
-if int(lista[0][10]) != validacao2:
-    print('CPF inválido3')
-else:
-    print('CPF totalmente validado com sucesso!')
+    print('CPF inválido!')
+
 
 
